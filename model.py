6dcc6a80f6b5a30bcbbf4302ce7aeb0eb2571e48
MasterHM-ml <masterhmhamza.ml@gmail.com>
2023-06-12 03:53:41 +0500

    resolved #4

--- model.py
+++ model.py
@@ -43,7 +43,7 @@ class ModelLosses:
 
 
 class DCGAN(object):
-  def __init__(self, tf_session, input_height=108, input_width=108, crop=True,
+  def __init__(self, tf_session=None, input_height=108, input_width=108, crop=True,
          batch_size=64, sample_num = 64, output_height=64, output_width=64,
          y_dim=None, z_dim=100, gf_dim=64, df_dim=64,
          gfc_dim=1024, dfc_dim=1024, c_dim=3, dataset_name='default',

f67016f35759e9bce1dcf3c556dc3ac7414c6e41
MasterHM-ml <masterhmhamza.ml@gmail.com>
2023-06-11 21:19:56 +0500

    a sperate if branch and method for built-in dataset, else branch in __init__ for images from directory, some arguments imporovement

--- model.py
+++ model.py
@@ -3,10 +3,11 @@ from __future__ import print_function
 import os
 import time
 import math
+import logging
+
 from glob import glob
 import tensorflow as tf
 from tensorflow.keras import layers  # pyright: ignore
-# import keras
 import numpy as np
 from six.moves import xrange # pyright: ignore
 
@@ -42,11 +43,11 @@ class DCGAN(object):
       dfc_dim: (optional) Dimension of discriminator units for fully connected layer. [1024]
       c_dim: (optional) Dimension of image color. For grayscale input, set to 1. [3]
     """
-    self.tf_session = tf_session
+    # self.tf_session = tf_session
     self.crop = crop
 
     self.batch_size = batch_size
-    self.sample_num = sample_num
+    # self.sample_num = sample_num
 
     self.input_height = input_height
     self.input_width = input_width
@@ -83,34 +84,40 @@ class DCGAN(object):
     self.out_dir = out_dir
     self.max_to_keep = max_to_keep
 
-    if self.dataset_name == 'mnist':
-      self.data_X, self.data_y = self.load_mnist()
+    if self.dataset_name in ["mnist", "fashion_mnist", "cifar10", "cifar100"]:
+      self.data_X, self.data_y = self.load_builtin_dataset()
       self.c_dim = self.data_X[0].shape[-1]
     else:
       data_path = os.path.join(self.data_dir, self.dataset_name, self.input_fname_pattern)
-      self.data = glob(data_path)
-      if len(self.data) == 0:
-        raise Exception("[!] No data found in '" + data_path + "'")
-      np.random.shuffle(self.data)
-      imreadImg = imread(self.data[0])
-      if len(imreadImg.shape) >= 3: #check if image is a non-grayscale image by checking channel number
-        self.c_dim = imread(self.data[0]).shape[-1]
+      logging.info("loading custom data from %s." % data_path)
+      logging.info("Please make sure all images are either RGB (3 channels) or grayscale (1 channels). Got argument 'c_dim'=%d" % self.c_dim)
+      self.data_X = glob(data_path)
+      if len(self.data_X) == 0: raise Exception("[!] No data found in '" + data_path + "'")
+      if len(self.data_X) < self.batch_size: raise Exception("[!] Entire dataset size is less than the configured batch_size")
+        
+      if self.c_dim==1:
+        self.data_X = np.stack([np.reshape(np.array(Image.open(x).convert('L')), (self.input_height, self.input_width)) for x in self.data_X])
+      elif self.c_dim==3:
+        self.data_X = np.stack([np.reshape(np.array(Image.open(x).convert('RGB')), (self.input_height, self.input_width, self.c_dim)) for x in self.data_X])
       else:
-        self.c_dim = 1
-
-    if len(self.data) < self.batch_size:
-      raise Exception("[!] Entire dataset size is less than the configured batch_size")
-    
-    self.grayscale = (self.c_dim == 1)
+        raise Exception("[!] Unknown color dimension. Got argument 'c_dim'=%d" % self.c_dim)
+      
+      np.random.shuffle(self.data_X)
+      self.data_X = (self.data_X - 127.5) / 127.5  # Normalize the images to [-1, 1]
+      self.buffer_size = self.data_X.shape[0]
+      self.data_y = np.ones(self.data_X.shape[0])
+      self.data_X = tf.data.Dataset.from_tensor_slices(self.data_X).shuffle(self.buffer_size).batch(self.batch_size)
+      
+    # self.grayscale = (self.c_dim == 1)
 
     self.build_model()
 
   def build_model(self):
-    if self.y_dim:
-      # self.y = tf.placeholder(tf.float32, [self.batch_size, self.y_dim], name='y')
-      self.y = tf.Variable(tf.zeros([self.batch_size, self.y_dim]), dtype=tf.float32)
-    else:
-      self.y = None
+    # if self.y_dim:
+    #   # self.y = tf.placeholder(tf.float32, [self.batch_size, self.y_dim], name='y')
+    #   self.y = tf.Variable(tf.zeros([self.batch_size, self.y_dim]), dtype=tf.float32)
+    # else:
+    #   self.y = None
 
     if self.crop:
       image_dims = [self.output_height, self.output_width, self.c_dim]
@@ -120,18 +127,17 @@ class DCGAN(object):
     # self.inputs = tf.placeholder(
     #   tf.float32, [self.batch_size] + image_dims, name='real_images')
     # self.inputs = tf.Variable(tf.zeros([self.batch_size]+image_dims), dtype=tf.floats32) # SYNTAX ERROR
-    self.inputs = tf.Variable(tf.zeros([self.batch_size]+image_dims), dtype=tf.float32)
+    # self.inputs = tf.Variable(tf.zeros([self.batch_size]+image_dims), dtype=tf.float32)
 
-    inputs = self.inputs
+    # inputs = self.inputs
 
     # self.z = tf.placeholder(
     #   tf.float32, [None, self.z_dim], name='z')
     self.z = tf.Variable(tf.zeros([None, self.z_dim]), dtype=tf.float32)
     self.z_sum = histogram_summary("z", self.z)
 
-    self.G: tf.keras.Sequential = self.generator(self.z, self.y)
-    self.D, self.D_logits = self.discriminator(inputs, self.y, reuse=False)
-    # self.sampler            = self.sampler(self.z, self.y)
+    self.G = self.generator(self.z)
+    self.D, self.D_logits = self.discriminator(image_dims=image_dims,)
     self.D_, self.D_logits_ = tf.keras.models.clone_model(self.D), tf.identity(self.D_logits)
     
     self.d_sum = histogram_summary("d", self.D)
@@ -163,7 +169,7 @@ class DCGAN(object):
     # self.d_vars = [var for var in t_vars if 'd_' in var.name]
     # self.g_vars = [var for var in t_vars if 'g_' in var.name]
 
-    self.saver = tf.train.Saver(max_to_keep=self.max_to_keep)
+    # self.saver = tf.train.Saver(max_to_keep=self.max_to_keep)
 
   def train(self, config):
     d_optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1) \
@@ -189,7 +195,7 @@ class DCGAN(object):
       sample_inputs = self.data_X[0:self.sample_num]
       sample_labels = self.data_y[0:self.sample_num]
     else:
-      sample_files = self.data[0:self.sample_num]
+      sample_files = self.data_X[0:self.sample_num]
       sample = [
           get_image(sample_file,
                     input_height=self.input_height,
@@ -216,17 +222,17 @@ class DCGAN(object):
       if config.dataset == 'mnist':
         batch_idxs = min(len(self.data_X), config.train_size) // config.batch_size
       else:      
-        self.data = glob(os.path.join(
+        self.data_X = glob(os.path.join(
           config.data_dir, config.dataset, self.input_fname_pattern))
-        np.random.shuffle(self.data)
-        batch_idxs = min(len(self.data), config.train_size) // config.batch_size
+        np.random.shuffle(self.data_X)
+        batch_idxs = min(len(self.data_X), config.train_size) // config.batch_size
 
       for idx in xrange(0, int(batch_idxs)):
         if config.dataset == 'mnist':
           batch_images = self.data_X[idx*config.batch_size:(idx+1)*config.batch_size]
           batch_labels = self.data_y[idx*config.batch_size:(idx+1)*config.batch_size]
         else:
-          batch_files = self.data[idx*config.batch_size:(idx+1)*config.batch_size]
+          batch_files = self.data_X[idx*config.batch_size:(idx+1)*config.batch_size]
           batch = [
               get_image(batch_file,
                         input_height=self.input_height,
@@ -365,9 +371,9 @@ class DCGAN(object):
         h3 = linear(h2, 1, 'd_h3_lin')
         
         return tf.nn.sigmoid(h3), h3
-  def discriminator(self, image,):
+  def discriminator(self, image_dims,):
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Conv2D(self.df_dim, (5,5), (2,2), "same", use_bias=True, input_shape=image.shape,
+    model.add(tf.keras.layers.Conv2D(self.df_dim, (5,5), (2,2), "same", use_bias=True, input_shape=image_dims,
                                      kernel_initializer=tf.keras.initializers.TruncatedNormal(mean=0.0, stddev=0.02, seed=None),
                                      bias_initializer=tf.keras.initializers.Constant(value=0),))
     model.add(tf.keras.layers.LeakyReLU())
@@ -465,7 +471,7 @@ class DCGAN(object):
 
     model = tf.keras.Sequential()
     
-    model.add(layers.Dense(self.gf_dim*8*s_h16*s_w16, use_bias=True, input_shape=[None, z.get_shape()[1]],
+    model.add(layers.Dense(self.gf_dim*8*s_h16*s_w16, use_bias=True, input_shape=z.get_shape()[1],
                            kernel_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.02, seed=None),
                            bias_initializer=tf.keras.initializers.Constant(value=0),))
     model.add(layers.BatchNormalization())
@@ -536,6 +542,23 @@ class DCGAN(object):
     #   return model
 
 
+  def load_builtin_dataset(self):
+    logging.info("Loading built-in dataset, input_height and input_width will be reset")
+    (train_images, _), (_, _) = getattr(tf.keras.datasets, self.dataset_name).load_data()
+    self.input_height = self.input_width = train_images[0].shape[0]
+    if train_images[0].shape[-1] == 3: # either shape will be HxW or HxWx3
+      self.c_dim = 3
+    else:
+      self.c_dim = 1
+    
+    train_images = train_images.reshape(train_images.shape[0], self.input_width, self.input_height, self.c_dim).astype('float32')
+    train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
+    self.buffer_size = train_images.shape[0]
+    test_dataset = np.ones(train_dataset.shape[0])
+    train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(self.buffer_size).batch(self.batch_size)
+    return train_dataset, test_dataset
+
+
   # def sampler(self, z, y=None):
   #   with tf.variable_scope("generator") as scope:
   #     scope.reuse_variables()
@@ -588,44 +611,9 @@ class DCGAN(object):
 
   #       return tf.nn.sigmoid(deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3'))
 
-  def load_mnist(self):
-    data_dir = os.path.join(self.data_dir, self.dataset_name)
-    
-    fd = open(os.path.join(data_dir,'train-images-idx3-ubyte'))
-    loaded = np.fromfile(file=fd,dtype=np.uint8)
-    trX = loaded[16:].reshape((60000,28,28,1)).astype(np.float)
 
-    fd = open(os.path.join(data_dir,'train-labels-idx1-ubyte'))
-    loaded = np.fromfile(file=fd,dtype=np.uint8)
-    trY = loaded[8:].reshape((60000)).astype(np.float)
 
-    fd = open(os.path.join(data_dir,'t10k-images-idx3-ubyte'))
-    loaded = np.fromfile(file=fd,dtype=np.uint8)
-    teX = loaded[16:].reshape((10000,28,28,1)).astype(np.float)
-
-    fd = open(os.path.join(data_dir,'t10k-labels-idx1-ubyte'))
-    loaded = np.fromfile(file=fd,dtype=np.uint8)
-    teY = loaded[8:].reshape((10000)).astype(np.float)
-
-    trY = np.asarray(trY)
-    teY = np.asarray(teY)
-    
-    X = np.concatenate((trX, teX), axis=0)
-    y = np.concatenate((trY, teY), axis=0).astype(np.int)
-    
-    seed = 547
-    np.random.seed(seed)
-    np.random.shuffle(X)
-    np.random.seed(seed)
-    np.random.shuffle(y)
-    
-    y_vec = np.zeros((len(y), self.y_dim), dtype=np.float)
-    for i, label in enumerate(y):
-      y_vec[i,y[i]] = 1.0
-    
-    return X/255.,y_vec
-
-  @property
+  # @property
   # def model_dir(self):
   #   return "{}_{}_{}_{}".format(
   #       self.dataset_name, self.batch_size,
